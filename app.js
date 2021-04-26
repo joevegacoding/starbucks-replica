7b6bd5b8afc79fa4f988f88ed2230c39cce89c68
joevegacoding <josephbouhanef@gmail.com>
2021-04-26 17:48:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
+
+$(".panel-collapse").on("show.bs.collapse", function () {
+  $(this).siblings(".panel-heading").addClass("active");
+});
+
+$(".panel-collapse").on("hide.bs.collapse", function () {
+  $(this).siblings(".panel-heading").removeClass("active");
+});
